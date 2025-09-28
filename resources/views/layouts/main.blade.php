84a8fabfe7582938bae4cd205c3cbc17c348c14f
dapzz-id <kadaviradityaa@outlook.co.id>
2025-09-28 13:30:02 +0700

    Test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,21 +13,6 @@
             position: relative;
         }
 
-        body::before {
-            content: '';
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-image:
-                linear-gradient(rgba(255, 255, 255, 0.1) 1px, transparent 1px),
-                linear-gradient(90deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px);
-            background-size: 20px 20px;
-            pointer-events: none;
-            z-index: 1;
-        }
-
         main {
             position: relative;
             z-index: 2;
